39e98cd8b3f63451aba3c609fe2c4869deca7291
meytosty <ketebay.b@gmail.com>
2023-05-01 06:41:30 +0600

    adding documentation to the MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -4,27 +4,61 @@ public class MyArrayListStack<T> {
     public MyArrayListStack() {
         this.list = new MyArrayList<>();
     }
+
+    /**
+     * isEmptyThrowException - throwing exception if stack is empty to avoid errors
+     * @noparam
+     * @return void
+     */
     private void isEmptyThrowException() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
     }
+
+    /**
+     * push - adding element to top of the stack
+     * @param item - given element to add
+     * @return void
+     */
     public void push(T item) {
         this.list.add(item, 0);
     }
 
+    /**
+     * pop - remove specified element and return element at the top of stack
+     * @noparam
+     * @return T - element in generic type
+     */
     public T pop() {
         isEmptyThrowException();
         return (T) list.remove(0);
     }
 
+    /**
+     * peek - returns the element at the top of the stack without removing it
+     * @noparam
+     * @return T - element in generic type
+     */
     public T peek() {
         isEmptyThrowException();
         return (T) list.get(0);
     }
+
+    /**
+     * isEmpty - returns true if the stack is empty or false if it is not
+     * @noparam
+     * @return boolean
+     */
     public boolean isEmpty() {
         return list.isEmpty();
     }
+
+    /**
+     * size - returns the number of elements in the stack
+     * @noparam
+     * @return int
+     */
     public int size() {
         return list.size();
     }
